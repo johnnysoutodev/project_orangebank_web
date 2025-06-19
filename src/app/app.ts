6c6d3b15b8a7feb62eec6e0737c480c349685075
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-06-19 01:04:26 -0300

    organizando estrutura dos componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Menu } from "./components/menu/menu";
+import { Hero } from './pages/hero/hero';
+import { About } from "./pages/about/about";
+import { Tech } from "./pages/tech/tech";
+import { Account } from "./pages/account/account";
+import { Card } from "./pages/card/card";
+import { Invest } from "./pages/invest/invest";
+import { Faq } from "./pages/faq/faq";
+import { Banner } from "./pages/banner/banner";
+import { Footer } from "./pages/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Menu, Hero, About, Tech, Account, Card, Invest, Faq, Banner, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
