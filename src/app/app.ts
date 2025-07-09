929ae6514f4cbaa95df1c30f2f3090fea9644075
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-07-09 03:00:57 -0300

    add new pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Menu } from "./components/menu/menu";
-import { Hero } from './pages/hero/hero';
+import { HeroComponent } from './pages/hero/hero';
 import { About } from "./pages/about/about";
 import { Tech } from "./pages/tech/tech";
 import { Account } from "./pages/account/account";
@@ -13,7 +13,7 @@ import { Footer } from "./pages/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Menu, Hero, About, Tech, Account, Card, Invest, Faq, Banner, Footer],
+  imports: [Header, Menu, HeroComponent, About, Tech, Account, Card, Invest, Faq, Banner, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
