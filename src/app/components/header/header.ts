7191cf3932b0affe6f065356f6b1848a99f74965
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-07-10 00:27:33 -0300

    adicionando logo no header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Button } from '../button/button';
+import { Logo } from '../logo/logo';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, Button],
+  imports: [CommonModule, Button, Logo],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
@@ -22,6 +23,12 @@ export class Header {
 
   onOpenAccount() {
     console.log('Abrir conta Orange');
+    this.closeMobileMenu();
+  }
+
+  onLogoClick() {
+    // Navegar para o início da página
+    window.scrollTo({ top: 0, behavior: 'smooth' });
     this.closeMobileMenu();
   }
 }
